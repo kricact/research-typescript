b271b420100523bfd78f967e146ff33fd090cad2
Kristina Pchelintseva <kristina.pchelintseva@jetbrains.com>
2024-05-10 01:01:17 +0200

    prepared project for research

---
+++ src/helloworld.ts
@@ -0,0 +1,8 @@
+interface MockMessage {
+  name: string;
+  age: number;
+}
+
+export function message(): string {
+  return "hello world!";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { message } from './helloworld';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,7 +7,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: message() });
 });
 
 app.listen(port, host, () => {
